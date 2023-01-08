7fdcc01604acf505b1a453e467da9ff722062831
Almat Juvashev <juvashev@gmail.com>
2023-01-08 22:38:19 +0600

    Added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+from django.conf import settings
+
 # Create your models here.
 class UserManager(BaseUserManager):
     """Manager for users"""
@@ -41,4 +43,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
 
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
 
+    def __str__(self):
+        return self.title
