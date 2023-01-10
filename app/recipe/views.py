3f266dec74c6cdd147e83adc7c0ed33334ab62d6
Almat <juvashev@gmail.com>
2023-01-10 09:13:59 +0600

    Configured recipe API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,7 +10,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -18,3 +18,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe.
+        Overrides the behaviour for when Django framework saves the view.
+        When we create a new recipe through the create method of the viewset this methods will be called.
+        This method sets the user field of the recipe to the current authenticated user
+        """
+        serializer.save(user=self.request.user)
+
